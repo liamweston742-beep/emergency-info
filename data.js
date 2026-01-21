a2ad72b7ca305c4e5cfe355a2161b95ac4328126
liamweston742-beep <liamweston742@gmail.com>
2026-01-21 18:05:24 +0000

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,2 @@
-
+const people = {
+  "test123": { passphrase: "testphrase", name: "Alex Smith", nationality: "UK", allergies: "Peanuts", medical: "Asthma", medications: "Salbutamol inhaler", blood: "O+", contact: "+44 7000000000", contact2: "nmnbvcv", languages: "English, French" }

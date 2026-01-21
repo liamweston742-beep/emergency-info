1f9596e2560d0ef187a59babfe8fa83fa133b574
liamweston742-beep <liamweston742@gmail.com>
2026-01-21 18:11:15 +0000

    Delete data.js

--- data.js
+++
@@ -1,2 +0,0 @@
-const people = {
-  "test123": { passphrase: "testphrase", name: "Alex Smith", nationality: "UK", allergies: "Peanuts", medical: "Asthma", medications: "Salbutamol inhaler", blood: "O+", contact: "+44 7000000000", contact2: "nmnbvcv", languages: "English, French" }
